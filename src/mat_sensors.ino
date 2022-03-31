6f6858df6e359056996e6300a90c2a82054a14a4
synneva <synneva-rcf@hotmail.com>
2022-03-31 12:12:10 -0400

    sketch setup

---
+++ src/mat_sensors.ino
@@ -0,0 +1,21 @@
+
+// 2x hx711 chips - 4 objects? (one per channel?)
+    // 1A = lateral strain
+    // 1B = extension
+    // 2A = axial strain
+    // 2B = loadcell
+        // chip 1 on pin 2, chip 2 on pin 3
+// lvdt analog in on A0
+// ls7366 - encoder on SPI (pins 10-13)
+
+
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
