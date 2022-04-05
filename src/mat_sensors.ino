63d51360b87fd940768408308190627c5faffdcf
synneva <synneva-rcf@hotmail.com>
2022-04-05 11:53:59 -0400

    encoder, lvdt functional, scales untested

--- src/mat_sensors.ino
+++ src/mat_sensors.ino
@@ -10,7 +10,7 @@
     // 2B = loadcell
         // chip 1 on pin 2, chip 2 on pin 3
 // lvdt analog in on A0
-// ls7366 - encoder on SPI (pins 10-13)
+// ls7366 - encoder on SPI (pins 10-13)     24MHz
 
 const int SCALE1_DOUT_PIN = 2;
 const int SCALE2_DOUT_PIN = 3;
@@ -19,35 +19,99 @@ const int ENCODER_SELECT_PIN = 10;
 const int LVDT_PIN = A0;
 
 
-HX711 scale1, scale2;
+HX711 scale1A, scale1B, scale2A, scale2B;
 LS7366 encoder(ENCODER_SELECT_PIN);
+
+long lvdt_value = 0;
+unsigned long encoder_count = 0;
+long scale1_reading = 0;
+long scale2_reading = 0;
 
 void setup() {
     Serial.begin(9600);
-    lvdt_setup();
+    //lvdt_setup();
     encoder_setup();
 
 
 }
 
 void loop() {
-
-
+    lvdt_read();
+    encoder_read();
 }
 
+void scale_setup() {
+    scale1A.begin(SCALE1_DOUT_PIN, SCALE_SCK_PIN, 64);
+    scale1B.begin(SCALE1_DOUT_PIN, SCALE_SCK_PIN, 32);
+    scale2A.begin(SCALE2_DOUT_PIN, SCALE_SCK_PIN, 64);
+    scale2B.begin(SCALE2_DOUT_PIN, SCALE_SCK_PIN, 32);
+}
+
+void scale_read(char channel) { // channel A for strain, B for extensiometer + load
+    switch(channel) {
+        case 'A':
+        case 'a':
+            if(scale1A.is_ready()){
+                scale1_reading = scale1A.read();
+                Serial.print("Lateral strain: ");
+                Serial.print(scale1_reading);
+                Serial.print("\n");
+            }
+            else
+                Serial.println("Scale 1 not found");
+
+            if(scale2A.is_ready()){
+                scale2_reading = scale2A.read();
+                Serial.print("Axial strain: ");
+                Serial.print(scale2_reading);
+                Serial.print("\n");
+            }
+            else
+                Serial.println("Scale 2 not found");
+
+            break;
+
+        case 'B':
+        case 'b':
+            if(scale1B.is_ready()){
+                scale1_reading = scale1B.read();
+                Serial.print("Extension: ");
+                Serial.print(scale1_reading);
+                Serial.print("\n");
+            }
+            else
+                Serial.println("Scale 1 not found");
+
+            if(scale2B.is_ready()){
+                scale2_reading = scale2B.read();
+                Serial.print("Load: ");
+                Serial.print(scale2_reading);
+                Serial.print("\n");
+            }
+            else
+                Serial.println("Scale 2 not found");
+
+            break;
+            
+        default:
+            Serial.println("Select channel A or B");
+    }
+}
+
+// not needed until circuit is tuned for full analog swing (currently from <0 to ~3.7V)
 void lvdt_setup(){
-    analogReference(EXTERNAL);
-    long sensorValue = 0;
+    //analogReference(EXTERNAL);
 }
 
 void lvdt_read(){
-    sensorValue = analogRead(sensorPin);
-    Serial.println(sensorValue);
-    delay(1000);
+    lvdt_value = analogRead(LVDT_PIN);
+    Serial.print("LVDT value: ");
+    Serial.print(lvdt_value);
+    Serial.print("\n");
+    delay(500);
 }
 
 void encoder_setup() {
-  Serial.begin(9600);
   encoder.write_mode_register_0(FILTER_1 | DISABLE_INDX | FREE_RUN | QUADRX4);
   encoder.write_mode_register_1(NO_FLAGS | EN_CNTR | BYTE_4 );
   encoder.clear_counter();
@@ -56,17 +120,16 @@ void encoder_setup() {
 }
 
 void encoder_read() {
-  unsigned long count = encoder.read_counter();
-  Serial.print("Count: ");
-  Serial.print(count, DEC);
-  Serial.print(" Status: ");
-  print_binary(encoder.read_status_register());
+  encoder_count = encoder.read_counter();
+  Serial.print("Encoder count: ");
+  Serial.print(encoder_count, DEC);
+  //Serial.print(" Status: ");
+  //print_binary(encoder.read_status_register());
   Serial.print("\n");
-  delay(1000);
+  delay(500);
 }
 
 //Function to print out one byte in a readable, left-padded binary format
-
 void print_binary(byte val)
 {
 byte i=0;
